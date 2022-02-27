900f05da31def7583385c5b793ded3221f552045
Beldine-Moturi <beldinemoturi@gmail.com>
2022-02-27 17:20:22 +0300

    [style]: fix code to conform to betty style

--- push.c
+++ push.c
@@ -19,8 +19,7 @@ void push(stack_t **stack, unsigned int line_number)
 	{
 		if ((isdigit(arg[i])) == 0)
 		{
-			fprintf(stderr, "L%d: usage: push integer\n",\
-				line_number);
+			fprintf(stderr, "L%d: usage: push integer\n", line_number);
 			exit(EXIT_FAILURE);
 		}
 	}
