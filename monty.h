bb92c8acfb7de7863a269977a565d5d62384f925
Beldine-Moturi <beldinemoturi@gmail.com>
2022-02-27 23:51:17 +0300

    [feat]: add function that adds the top two elements of the stack
    add: adds the top two elements of the stack:
    The result is stored in the second top element of the stack, and the top element is removed, so that at the end:
    The top element of the stack contains the result
    The stack is one element shorter

--- monty.h
+++ monty.h
@@ -18,6 +18,7 @@
 		{"pint", pint},			\
 		{"pop", pop},			\
 		{"swap", swap},			\
+		{"add", add},			\
 		{NULL, NULL}			\
 	}
 
@@ -79,6 +80,7 @@ void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 
 #endif
