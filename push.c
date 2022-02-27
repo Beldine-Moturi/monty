45b642192248bbce91650ad0e079c045ddbd39b6
Beldine-Moturi <beldinemoturi@gmail.com>
2022-02-27 16:35:45 +0300

    [feat]: add push and pall opcode functions
    push: a function to push an element onto the stack
    pall: a function to print all elements in the stack, from the top

---
+++ push.c
@@ -0,0 +1,41 @@
+#include "monty.h"
+
+/**
+ *push - pushes an element onto a stack
+ *@stack: the stack to push the element to
+ *@line_number: the line number of the particulare opcode instruction
+ *              in the monty byte code file
+ *
+ *Return: void
+ */
+
+void push(stack_t **stack, unsigned int line_number)
+{
+	char *arg = Arg.argument;
+	int data, i;
+	stack_t *element;
+
+	for (i = 0; arg[i] != '\0'; i++)
+	{
+		if ((isdigit(arg[i])) == 0)
+		{
+			fprintf(stderr, "L%d: usage: push integer\n",\
+				line_number);
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	data = atoi(arg);
+	element = malloc(sizeof(stack_t));
+	if (element == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	element->n = data;
+	element->prev = NULL;
+	element->next = *stack;
+	if (*stack != NULL)
+		(*stack)->prev = element;
+	*stack = element;
+}
